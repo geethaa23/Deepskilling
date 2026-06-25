ece8dba2ad0b396ea6e0ae430a2c8ab3d8f673cd
Geetha Akshaya <geethaakshaya2325@gmail.com>
2026-06-25 19:37:48 +0530

    Completed Singleton Pattern

---
+++ 01-Design-Patterns/SingletonPattern/Program.cs
@@ -0,0 +1,14 @@
+﻿Logger logger1 = Logger.GetInstance();
+Logger logger2 = Logger.GetInstance();
+
+logger1.Log("First log message");
+logger2.Log("Second log message");
+
+if (logger1 == logger2)
+{
+    Console.WriteLine("Only one Logger instance exists.");
+}
+else
+{
+    Console.WriteLine("Multiple Logger instances exist.");
+}
