93c1a30d34b33b7e3aae9a954c14ed60ad3ba083
Geetha Akshaya <geethaakshaya2325@gmail.com>
2026-06-28 21:17:17 +0530

    Completed Observer Pattern

---
+++ 01-Design-Patterns/ObserverPattern/Program.cs
@@ -0,0 +1,9 @@
+﻿StockMarket stockMarket = new StockMarket();
+
+IObserver mobile = new MobileApp();
+IObserver web = new WebApp();
+
+stockMarket.RegisterObserver(mobile);
+stockMarket.RegisterObserver(web);
+
+stockMarket.SetStock("TCS", 3560.75);
