5c78f8f677acde2bdefddde875c7d112a9864135
Geetha Akshaya <geethaakshaya2325@gmail.com>
2026-06-28 21:38:34 +0530

    Completed Financial Forecasting

---
+++ 02-Data-Structures-And-Algorithms/FinancialForecasting/Program.cs
@@ -0,0 +1,10 @@
+﻿double currentValue = 10000;
+double growthRate = 0.10;   // 10%
+int years = 5;
+
+double futureValue = FinancialForecast.PredictFutureValue(currentValue, growthRate, years);
+
+Console.WriteLine($"Current Value : ₹{currentValue}");
+Console.WriteLine($"Growth Rate   : {growthRate * 100}%");
+Console.WriteLine($"Years         : {years}");
+Console.WriteLine($"Future Value  : ₹{futureValue:F2}");
