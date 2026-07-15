348280cc3ca8a1c2eb9eb5c1e24e727b8e5fd650
Geetha Akshaya <geethaakshaya2325@gmail.com>
2026-07-15 15:47:16 +0530

    Completed NUnit Hands-on

---
+++ 04-NUnit-Moq/CalculatorTests/CalculatorTests.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using CalcLibrary;
+
+namespace CalculatorTests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        private Calculator calculator;
+
+        [SetUp]
+        public void Setup()
+        {
+            calculator = new Calculator();
+        }
+
+        [TearDown]
+        public void Cleanup()
+        {
+            calculator = null;
+        }
+
+        [TestCase(2, 3, 5)]
+        [TestCase(10, 15, 25)]
+        [TestCase(-5, 5, 0)]
+        public void TestAddition(int a, int b, int expected)
+        {
+            int actual = calculator.Add(a, b);
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+    }
+}
